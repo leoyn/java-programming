8511aad07e4e3f63fa527a567e54d8b8e74769d6
leuchan <leo@mixify.ga>
2019-11-26 18:29:42 +0100

    Changed from while to for loop

--- src/semester1/chapter4/Task5.java
+++ src/semester1/chapter4/Task5.java
@@ -7,18 +7,16 @@ public class Task5 {
 		
 		System.out.print("x = ");
 		int x = ioScanner.nextInt();
-		int i = 0;
 		ioScanner.close();
 		
 		// example for 10
-		while(x > 0) {
+		for(int i = 0; x > 0; i++) {
 			
 			if(x % 2 == 1) { // 10 / 2 = 5 R0; 5 / 2 = 2 R1; 2 / 2 = 1 R0; 1 / 2 = 0 R1;
 				System.out.print(i + ", "); // x % 2 == 0; print 1; x % 2 == 0; print 3;
 			}
 			
 			x = x / 2; // 10 / 2 = 5; 5 / 2 = 2; 2 / 2 = 1; 1 / 2 = 0;
-			i++; // i = 1; i = 2; i = 3; i = 4;
 		}
 	}
 }
